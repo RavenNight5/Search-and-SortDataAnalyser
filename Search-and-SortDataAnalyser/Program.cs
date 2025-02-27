a49619149f85b7f4cf8d6c182c1e63a747b9353b
RavenNight5 <182237149+RavenNight5@users.noreply.github.com>
2025-02-27 10:22:10 +0000

    Text file can now be read and written to the console.

--- Search-and-SortDataAnalyser/Program.cs
+++ Search-and-SortDataAnalyser/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,27 @@ namespace Search_and_SortDataAnalyser
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
+            string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            Console.Write(sCurrentDirectory);
+
+            Console.ReadKey();
+
+            string sFile = Path.Combine(sCurrentDirectory, @"..\..\..\Search-and-SortDataAnalyser\Data_256\Share_1_256.txt");
+            string sFilePath = Path.GetFullPath(sFile);
+
+            Console.Write("sFile : " + sFile);
+            Console.Write("sFilePath : " + sFilePath);
+            
+            Console.ReadKey();
+
+            string contents = File.ReadAllText(sFilePath);
+
+            Console.Write(contents);
+            
+            Console.ReadKey();
         }
     }
 }
